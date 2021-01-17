433eedcc46be8ce689e8ca05a823b03fbd9aeb75
Alameer <ayataharimawi@gmail.com>
2021-01-17 11:41:58 +0200

    virsion 0.1

--- app/src/main/java/edu/cs/birzeit/mobileapp/MainActivity2.java
+++ app/src/main/java/edu/cs/birzeit/mobileapp/MainActivity2.java
@@ -1,16 +1,20 @@
 package edu.cs.birzeit.mobileapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity2 extends AppCompatActivity {
-
+    RecyclerView recycler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        recycler=findViewById(R.id.phone_recycler);
     }
+
+
 
 
 }
---
+++ app/src/main/java/edu/cs/birzeit/mobileapp/phonedet.java
@@ -0,0 +1,24 @@
+package edu.cs.birzeit.mobileapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+public class phonedet extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phonedet);
+
+//        TextView txtDesc = (TextView) findViewById(R.id.txtDesc);
+//        if(getIntent().hasExtra("des")) {
+//            String de = getIntent().getStringExtra("des");
+//            txtDesc.setText(de);
+//        }
+    }
+}
